983b9111b729bb558ef8e07c60b5031f52f94386
cnmurakami <caionmurakami@gmail.com>
2024-11-15 19:46:01 -0300

    Initial commit

---
+++ src/test/java/dio/cloud/CloudApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
